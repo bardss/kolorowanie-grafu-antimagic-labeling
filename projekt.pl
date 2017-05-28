40149e2193d90994e739a9413361ad43decf2d62
Jakub Anioła <jakub.aniola@rsqtechnologies.com>
2017-05-28 13:25:58 +0200

    pierwsze proby wpisywania i czytania tekstu

---
+++ projekt.pl
@@ -0,0 +1,19 @@
+readFile():-
+	open('tekst.txt', read, Stream),
+	readFile(Stream,Lines),
+	close(Stream),!,
+	write(Lines).
+
+readFile(Stream,[]) :-
+    at_end_of_stream(Stream).
+
+readFile(Stream,[X|L]) :-
+    \+ at_end_of_stream(Stream),
+    read(Stream,X),
+    readFile(Stream,L).
+
+writeFile():-
+	open('hello.txt',write,Stream), 
+    write(Stream,'wpisany tekst'),  nl(Stream), 
+    close(Stream),
+
